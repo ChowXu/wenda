fa250ae9faee1ca6acbbd266133a92069d86442b
Jone212 <15651342081@163.com>
2017-02-14 16:24:20 +0800

    commit for loginInterceptor and homePage

---
+++ src/main/java/com/nowcoder/configuration/WendaWebConfiguration.java
@@ -0,0 +1,30 @@
+package com.nowcoder.configuration;
+
+import com.nowcoder.interceptor.LoginRequiredInterceptor;
+import com.nowcoder.interceptor.PassportInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by nowcoder on 2016/7/3.
+ */
+@Component
+public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    PassportInterceptor passportInterceptor;
+
+    @Autowired
+    LoginRequiredInterceptor loginRequiredInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截器注册器
+        //注册
+        registry.addInterceptor(passportInterceptor);
+        //给需要登录的目录 添加登录拦截器
+        registry.addInterceptor(loginRequiredInterceptor).addPathPatterns("/user/*");
+        super.addInterceptors(registry);
+    }
+}
